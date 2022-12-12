05927475228d1025f175d469e8939bab0d99afdd
sahilansarimondal <mondalrajesh761@gmail.com>
2022-12-12 18:07:48 +0530

    update

--- src/routes/route.js
+++ src/routes/route.js
@@ -13,6 +13,71 @@ router.get("/profile-details", function(req, res){
     res.send('dummy response')
 })
 
+router.get("/get/movies", function (req, res) {
+    const arr = ["Rang de basanti", "The shining", "Lord of the rings", "Batman begins"]
+    console.log("movies");
+    res.send(arr)
+})
+
+// router.get("/get/movies/:indexNumber", function (req, res) {
+//     const arr = ["Rang de basanti", "The shining", "Lord of the rings", "Batman begins"]
+//     let i = req.params.indexNumber
+//     console.log("movies index");
+//     res.send(arr[i])
+// })
+
+router.get("/get/movies/:indexNumber", function (req, res) {
+    const arr = ["Rang de basanti", "The shining", "Lord of the rings", "Batman begins"]
+    let i = req.params.indexNumber
+    console.log("invalid movies index");
+    if(i < arr.length){
+        res.send(arr[i])
+    }else{
+        res.send("Use a valid index")
+    }
+})
+
+router.get("/get/flims", function (req, res) {
+    const arr2 = [ {
+        "id": 1,
+        "name": "The Shining"
+       }, {
+        "id": 2,
+        "name": "Incendies"
+       }, {
+        "id": 3,
+        "name": "Rang de Basanti"
+       }, {
+        "id": 4,
+        "name": "Finding Nemo"
+       }]
+       console.log("movie obj")
+       res.send(arr2)  
+})
+
+router.get("/get/flims/:flimId", function (req, res) {
+    const arr2 = [ {
+        "id": 1,
+        "name": "The Shining"
+       }, {
+        "id": 2,
+        "name": "Incendies"
+       }, {
+        "id": 3,
+        "name": "Rang de Basanti"
+       }, {
+        "id": 4,
+        "name": "Finding Nemo"
+       }]
+       let i = req.params.flimId
+       console.log("movie obj")
+       if(i < arr2.length){
+           res.send(arr2[i - 1])  
+       }else{
+        res.send("No movie exists with this id")
+       }
+})
+
 router.get('/test-me', function (req, res) {
     console.log("email from introduction module", intro.myEmail)
     intro.myFunction('Sabiha')
